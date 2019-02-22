d40a381d2b53510ef3b29e249fd1a7b930482d6a
Marko Curic <mcuric1710@gmail.com>
2019-02-22 17:17:52 +0100

    Updated JS code and placed correct image paths

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-var image = document.getElementsByClassName("column");
+var upperImage = document.getElementsByClassName("upper column");
+var lowerImage = document.getElementsByClassName("lower column");
+
+console.log(upperImage);
+console.log(lowerImage);
 
 $(document).ready(function() {
 	$( "#LeftButton" ).click(function() {
-		for (let i=0; i<image.length;i++) {
-			image[i].style.order -= 1;
-			if(image[i].style.order == 0) image[i].style.order = 9;
+		for (let i=0; i<upperImage.length;i++) {
+			upperImage[i].style.order -= 1;
+			if(upperImage[i].style.order == 0) upperImage[i].style.order = upperImage.length;
+		};
+		for (let k=0; k<lowerImage.length;k++) {
+			lowerImage[k].style.order -= 1;
+			if(lowerImage[k].style.order == 0) lowerImage[k].style.order = lowerImage.length;
 		};
 	});
 
 	$( "#RightButton" ).click(function() {
-		for (let j=0; j<image.length;j++) {
-			image[j].style.order++;
-			if(image[j].style.order == 10) image[j].style.order = 1;
+		for (let j=0; j<upperImage.length;j++) {
+			upperImage[j].style.order++;
+			if(upperImage[j].style.order == upperImage.length + 1) upperImage[j].style.order = 1;
+		};
+		for (let l=0; l<lowerImage.length;l++) {
+			lowerImage[l].style.order++;
+			if(lowerImage[l].style.order == lowerImage.length + 1) lowerImage[l].style.order = 1;
 		};
 	});
 });
